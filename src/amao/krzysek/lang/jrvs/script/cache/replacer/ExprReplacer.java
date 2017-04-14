967f4a9df03305218c7898f90f1419935699c1e2
krzysek <loonypl@amao.pl>
2017-04-14 11:41:18 +0200

    Jrvs Script (Lang) update 8 - Fix
    Fixed bug when checking expression pattern.

--- src/amao/krzysek/lang/jrvs/script/cache/replacer/ExprReplacer.java
+++ src/amao/krzysek/lang/jrvs/script/cache/replacer/ExprReplacer.java
@@ -28,24 +28,36 @@ public class ExprReplacer {
                     String name = split[0];
                     String exprIn = StringUtils.substringBetween(replace, "%", "%");
                     if (exprIn != null) {
-                        if (exprIn.contains(name)) {
-                            String[] args = StringUtils.substringsBetween(exprIn, "\"", "\"");
-                            LinkedList<Object> rawArgs = new LinkedList<>();
-                            if (args != null) rawArgs.addAll(Arrays.asList(args));
-                            Class c = this.exprs.get(expr);
-                            Object obj = null;
-                            try {
+                        String exprInName = null;
+                        if (exprIn.contains("\"")) {
+                            exprInName = "";
+                            for (int i = 0; i < exprIn.length(); i++) {
+                                if (String.valueOf(exprIn.charAt(i)).equalsIgnoreCase("\"")) break;
+                                else exprInName = exprInName + String.valueOf(exprIn.charAt(i));
+                            }
+                            exprInName = exprInName.substring(0, exprInName.length()-1);
+                        } else if (!(exprIn.contains("\""))) exprInName = exprIn;
+                        if (exprInName != null) {
+                            if (exprInName.equalsIgnoreCase(name)) {
+                                String[] args = StringUtils.substringsBetween(exprIn, "\"", "\"");
+                                LinkedList<Object> rawArgs = new LinkedList<>();
+                                if (args != null) rawArgs.addAll(Arrays.asList(args));
+                                Class c = this.exprs.get(expr);
+                                Object obj = null;
                                 try {
-                                    obj = c.getMethod("run", LinkedList.class).invoke(null, rawArgs);
-                                } catch (IllegalAccessException e) {
-                                    e.printStackTrace();
-                                } catch (InvocationTargetException e) {
-                                    e.printStackTrace();
-                                } catch (NoSuchMethodException e) {
-                                    e.printStackTrace();
+                                    try {
+                                        obj = c.getMethod("run", LinkedList.class).invoke(null, rawArgs);
+                                    } catch (IllegalAccessException e) {
+                                        e.printStackTrace();
+                                    } catch (InvocationTargetException e) {
+                                        e.printStackTrace();
+                                    } catch (NoSuchMethodException e) {
+                                        e.printStackTrace();
+                                    }
+                                } finally {
+                                    for (int i = 0; i < script.size(); i++)
+                                        script.set(i, script.get(i).replaceAll("%" + exprIn + "%", String.valueOf(obj)));
                                 }
-                            } finally {
-                                for (int i = 0; i < script.size(); i++) script.set(i, script.get(i).replaceAll("%" + exprIn + "%", String.valueOf(obj)));
                             }
                         }
                     }
